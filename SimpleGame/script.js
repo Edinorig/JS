9352565472efbc2b40bd5864185cc1452f08f0d7
Edinorig <super-ddred@ukr.net>
2022-07-15 10:40:53 +0200

    test

---
+++ SimpleGame/script.js
@@ -0,0 +1,29 @@
+const dino   = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+
+document.addEventListener("keydown", function(event)
+{
+    jump();
+});
+
+function jump ()
+{
+    if(dino.classList != "jump")
+    {
+        dino.classList.add("jump")
+    }
+    setTimeout(function(){   //визиває один раз функцію через тот проміжок часу
+        dino.classList.remove("jump")
+    }, 300)
+}
+
+let isAlive = setInterval (function(){ //позваляє визивати функцію регулярно через тот часу
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140)
+    {
+        alert("Game over")
+    }
+},10)
